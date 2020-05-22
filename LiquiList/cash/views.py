8e0cca4db1bfd6dd0d72f60536d718833c837243
Ben Dahmen <bendahmen@gmail.com>
2020-05-22 15:10:10 +0200

    implemented a navbar

--- LiquiList/cash/views.py
+++ LiquiList/cash/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Welcome to LiquiList!')
+    return render(request, 'home.html')
