716cf2807b2d01496678c837ac98587bebdd7063
Ben Dahmen <bendahmen@gmail.com>
2020-05-23 20:28:55 +0200

    finished cw preview and added models

--- LiquiList/cash/admin.py
+++ LiquiList/cash/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Payable, Receivable, Recurrent, Account, PlanStatus, TypeStatus
 
 # Register your models here.
+admin.site.register(Payable)
+admin.site.register(Receivable)
+admin.site.register(Recurrent)
+admin.site.register(Account)
+admin.site.register(PlanStatus)
+admin.site.register(TypeStatus)
--- LiquiList/cash/models.py
+++ LiquiList/cash/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=60)
+    balance = models.IntegerField()
+    updateCW = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Receivable(models.Model):
+    amount = models.IntegerField()
+    customer = models.CharField(max_length=60)
+    week = models.IntegerField()
+    account = models.ForeignKey(Account, related_name='receivables', on_delete=models.CASCADE)
+
+class Recurrent(models.Model):
+    amount = models.IntegerField()
+    customer = models.CharField(max_length=60)
+    frequency = models.IntegerField()
+    account = models.ForeignKey(Account, related_name='recurrents', on_delete=models.CASCADE)
+
+class PlanStatus(models.Model):
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.status
+
+class TypeStatus(models.Model):
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.status
+
+class Payable(models.Model):
+    """ payment item """
+    amount = models.IntegerField()
+    customer = models.CharField(max_length=60)
+    week = models.IntegerField()
+    planStatus = models.ForeignKey(PlanStatus, related_name='payables', on_delete=models.CASCADE)
+    typeStatus = models.ForeignKey(TypeStatus, related_name='payables', on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, related_name='payables', on_delete=models.CASCADE)
