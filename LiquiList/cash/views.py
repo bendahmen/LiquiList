20a98e75645fd6810e9dd2c95da82130fbda4631
Ben Dahmen <bendahmen@gmail.com>
2020-05-24 13:04:41 +0200

    created template for cwView and generated loop to display receivables

--- LiquiList/cash/views.py
+++ LiquiList/cash/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 def home(request):
-    return render(request, 'home.html')
+    """ Calendar Weeks payment overview """
+    receivables = Receivable.objects.all()
+    return render(request, 'cwView.html', {'receivables': receivables})
