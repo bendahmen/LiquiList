d9a107df45a66707aead547d651ebb99de5187fa
bendahmen <bendahmen@gmail.com>
2020-06-22 16:43:18 +0200

    Implemented functionality to switch payments between weeks

--- LiquiList/cash/views.py
+++ LiquiList/cash/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
 
 def home(request):
     """ Calendar Weeks payments overview """
+
+
+    #Post request
+    if request.method == 'POST':
+        paymentID = request.POST['paymentID']
+        cw = request.POST['cwxxSelector']
+        paymentType = request.POST['paymentTypeForm']
+
+        if paymentType == 'receivables':
+            currentPayment = Receivable.objects.get(pk = paymentID)
+            currentPayment.week = cw
+            currentPayment.save()
+        else:
+            currentPayment = Payable.objects.get(pk = paymentID)
+            currentPayment.week = cw
+            currentPayment.save()
+
+        return redirect('home')
+
+
+
+    accounts = Account.objects.all()
+
     cWeeks = {1: dict(), 2: dict(), 3: dict(), 4: dict()}
     for week in range(1, 5):
-        cWeeks[week]['receivables'] = Receivable.objects.all()
-        cWeeks[week]['payables'] = Payable.objects.all()
-    return render(request, 'cwView.html', {'cWeeks': cWeeks})
+        cWeeks[week]['receivables'] = Receivable.objects.filter(week=week)
+        cWeeks[week]['payables'] = Payable.objects.filter(week=week)
+    return render(request, 'cwView.html', {'cWeeks': cWeeks, 'accounts': accounts})
